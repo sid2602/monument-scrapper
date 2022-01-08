8ef3a10258572a6bbc689a46f5e39ce7c663dfd8
Filip Kornaus <filip.kornaus@futurum.tech>
2022-01-08 16:14:43 +0100

    agree fro credential

--- main.ts
+++ main.ts
@@ -1,7 +1,29 @@
 const puppeteer = require("puppeteer");
 
-const main = () => {
-  console.log("hi node");
+const pageLink = "https://www.google.com/travel/";
+
+const configurePage = async (browser, url) => {
+  const page = await browser.newPage();
+  await page.goto(url);
+  return page;
+};
+
+const agreeForCredential = async (page) => {
+  const [button] = await page.$x("//*[text()[contains(.,'I agree')]]");
+
+  if (button) {
+    button.click();
+  }
+};
+
+const main = async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await configurePage(browser, pageLink);
+  await agreeForCredential(page);
+
+  setTimeout(() => {
+    browser.close();
+  }, 1000000);
 };
 
 main();
